8e2fd50eeeef84291b7f231966dafa5bc6f71691
Jori <jvalkenhoff@outlook.com>
2020-10-29 00:00:47 +0100

    Added colors, sass, fonts, testing out

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './sass/main.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <header className="App-header">
         <p>
           The site is currently under construction. 
           Please come back later...
         </p>
-
+  
         <p>
-          Check out my <a target="_blank" rel="noreferrer" href="https://nl.linkedin.com/in/joris-valkenhoff-22670a186">LinkedIn</a> page
+          Check out my <a target="_blank" rel="noreferrer" className="test-anchor" href="https://nl.linkedin.com/in/joris-valkenhoff-22670a186">LinkedIn</a> page
         </p>
+
+        <div className="testDiv">
+          <div className="color1"></div>
+          <div className="color2"></div>
+          <div className="color3"></div>
+          <div className="color4"></div>
+        </div>
       </header>
     </div>
   );
