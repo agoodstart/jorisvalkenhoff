4efece2d14ee1a841c2304747080031ab96adbd8
Jori <jvalkenhoff@outlook.com>
2020-10-28 13:34:55 +0100

    creating pwa version

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The site is currently under construction. 
+          Please come back later
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>
+          Check out my <a target="_blank" rel="noreferrer" href="https://nl.linkedin.com/in/joris-valkenhoff-22670a186">LinkedIn</a> page
+        </p>
       </header>
     </div>
   );
