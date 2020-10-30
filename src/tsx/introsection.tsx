38f437c02eb574b2425f88182455e7f328986515
Jori <jvalkenhoff@outlook.com>
2020-10-30 08:00:59 +0100

    colors choosing

---
+++ src/tsx/introsection.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const IntroSection: React.FC = () => {
+    return (
+        <section className="App"> 
+            <header className="App-header">
+                <h1>
+                    Test
+                </h1>
+            <p>
+                The site is currently under construction. 
+                Please come back later...
+            </p>
+        
+            <p>
+                Check out my <a target="_blank" rel="noreferrer" className="test-anchor" href="https://nl.linkedin.com/in/joris-valkenhoff-22670a186">LinkedIn</a> page
+            </p>
+            </header>
+      </section>
+    )
+}
