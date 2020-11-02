aac716f0f679b002f3bfea5ae77d9106ee41da60
Jori <jvalkenhoff@outlook.com>
2020-11-02 22:38:17 +0100

    Adding header

--- src/tsx/components/navbar.tsx
+++ src/tsx/components/navbar.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+
+
+type Navigation = {
+    links: string[]
+}
+
 export const Navbar: React.FC = () => {
+    const [logo, setLogo] = React.useState();
+
     return (
         <header className="header">
             <nav className="header--nav">
+                {}
                 <a className="header--nav__about" href="#">About</a>
                 <a className="header--nav__portfolio" href="#">Portfolio</a>
                 <a className="header--nav__contact" href="#">Contact</a>
--- src/tsx/introsection.tsx
+++ src/tsx/introsection.tsx
@@ -4,8 +4,16 @@ export const IntroSection: React.FC = () => {
     return (
         <div className="section-intro u-center-tex">
             <h1 className="h1">
-                {/* Coming soon... */}
+                This application is in development, using:
             </h1>
+            <li className="list">
+                <ul>Typescipt React</ul>
+                <ul>NextJS</ul>
+                <ul>Sass</ul>
+                <ul>Docker & Kubernetes</ul>
+                <ul>AWS</ul>
+                <ul>Apollo & GraphQL</ul>
+            </li>
         </div>
     )
 }
